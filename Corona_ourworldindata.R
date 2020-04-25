8e63174ab0a0941b67e38d411f38317b75d9ba22
Etienne Pauthenet <etienne.pauthenet@gmail.com>
2020-04-25 12:55:53 +0200

    Update Corona_ourworldindata.R

--- Corona_ourworldindata.R
+++ Corona_ourworldindata.R
@@ -5,7 +5,7 @@ library(directlabels)
 library(gridExtra)
 library(grid)
 
-setwd("~/Documents/R/CoronaVirus/Ourworldindata")
+#Load data
 url = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
 destfile = "~/Documents/R/CoronaVirus/Ourworldindata/owid-covid-data.csv"
 download.file(url,destfile)
@@ -15,17 +15,19 @@ df$location = as.factor(df$location)
 levels(df$location)[levels(df$location)=="United States"] <- "US"
 levels(df$location)[levels(df$location)=="United Kingdom"] <- "UK"
 #
+#Define different zooms
+n1 = 10000 ; n2 = 3000 ; n3 = 1000
 deaths_max = by(df$total_deaths,df$location,max)
-df1 = df[df$location %in% names(which(deaths_max>10000)),]
-df2 = df[df$location %in% names(which(deaths_max<=10000 & deaths_max>3000)),]
-df3 = df[df$location %in% names(which(deaths_max<=3000 & deaths_max>1000)),]
-df4 = df[df$location %in% names(which(deaths_max<=1000)),] 
+df1 = df[df$location %in% names(which(deaths_max>n1)),]
+df2 = df[df$location %in% names(which(deaths_max<=n1 & deaths_max>n2)),]
+df3 = df[df$location %in% names(which(deaths_max<=n2 & deaths_max>n3)),]
+df4 = df[df$location %in% names(which(deaths_max<=n3)),] 
 
+#Define different zooms
 df_rect=data.frame(x1 = as.Date(c("2020-03-25","2020-03-31"))
   ,x2 = c(as.Date(c("2020-05-20","2020-05-20")))
   ,y1 = c(0,0)
-  ,y2 = c(10000,3000)
-  ,t  = c(1,2))
+  ,y2 = c(n1,n2))
 
 pal = c("firebrick","orange","steelblue","grey")
 p1 = ggplot() +
@@ -60,22 +62,4 @@ p3 = ggplot() +
 print(p3)
 p4 = grid.arrange(p1,p2,p3,ncol=3, nrow=1, widths=c(4, 2, 2), heights=c(4))
 ggsave(plot=p4, filename=paste("Countries_zoom_",df1$date[dim(df1)[1]],".png",sep = ""), height=6, width=12)
-
-
-
-##########################
-p1 = ggplot() +
-  labs(title = paste("Covid-19 deaths by countries (",df$date[length(df$date)],")",sep = ""), subtitle = "source : https://covid.ourworldindata.org") +
-  xlab("") + ylab("deaths") + xlim(as.Date(c("2020-01-01","2020-05-10"))) + 
-  scale_y_continuous(trans = "log10",limits = c(1,100000),labels = scales::comma) +
-  theme_minimal() + theme(legend.position = "none") +
-  geom_line(data = df4,aes(x = date, y = total_deaths, group = location),colour = pal[4]) +
-  geom_line(data = df3,aes(x = date, y = total_deaths, group = location),colour = pal[3]) +
-  geom_line(data = df2,aes(x = date, y = total_deaths, group = location),colour = pal[2]) +
-  geom_line(data = df1,aes(x = date, y = total_deaths, group = location),colour = pal[1]) +
-  geom_dl(data = df1,aes(x = date, y = total_deaths,label = location),colour = pal[1], method = list(dl.trans(x = x + .2),"last.bumpup")) +
-  geom_dl(data = df2,aes(x = date, y = total_deaths,label = location),colour = pal[2], method = list(dl.trans(x = x + .2),"last.bumpup")) +
-  geom_dl(data = df3,aes(x = date, y = total_deaths,label = location),colour = pal[3], method = list(dl.trans(x = x + .2),"last.bumpup"))
-print(p1)
-ggsave(plot=p1, filename=paste("Countries_log_",df1$date[dim(df1)[1]],".png",sep = ""), height=10, width=10)
 
